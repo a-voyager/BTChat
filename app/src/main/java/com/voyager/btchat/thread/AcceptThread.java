0d2a7b9824b81125fd86a95f736bead395b0e815
wuhaojie <w19961009@126.com>
2015-10-28 23:47:47 +0800

    add constant interface.

--- app/src/main/java/com/voyager/btchat/thread/AcceptThread.java
+++ app/src/main/java/com/voyager/btchat/thread/AcceptThread.java
@@ -1,7 +1,31 @@
 package com.voyager.btchat.thread;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+
+import com.voyager.btchat.Utils.Constant;
+
+import java.io.IOException;
+
 /**
  * Created by wuhaojie on 2015/10/28.
  */
-public class AcceptThread {
+public class AcceptThread extends Thread {
+    private final BluetoothServerSocket mServerSocket = null;
+    //private BluetoothAdapter mAdapter;
+    private String mSocketType;
+
+    public AcceptThread(boolean secure, BluetoothAdapter mAdapter) {
+        BluetoothServerSocket tmp = null;
+        mSocketType = secure ? "secure" : "Insecure";
+
+        try {
+            if (secure) {
+                tmp = mAdapter.listenUsingRfcommWithServiceRecord(Constant.NAME_SECURE, Constant.MY_UUID_SECURE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
